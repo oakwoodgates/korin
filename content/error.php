f9e84657361994db3810785be045861887436d1c
oakwoodgates <funkyoaktree@gmail.com>
2015-09-12 09:31:43 -0400

    initial boom

---
+++ content/error.php
@@ -0,0 +1,11 @@
+<article <?php hybrid_attr( 'post' ); ?>>
+
+	<header class="entry-header">
+		<h1 class="entry-title"><?php esc_html_e( 'Nothing found', 'korin' ); ?></h1>
+	</header><!-- .entry-header -->
+
+	<div <?php hybrid_attr( 'entry-content' ); ?>>
+		<?php echo wpautop( esc_html__( 'Apologies, but no entries were found.', 'korin' ) ); ?>
+	</div><!-- .entry-content -->
+
+</article><!-- .entry -->
